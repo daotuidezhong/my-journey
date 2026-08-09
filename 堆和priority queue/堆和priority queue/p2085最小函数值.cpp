862637ca15278d1ff3ea8bae06ade7ed0125553a
daotuidezhong <241579381+daotuidezhong@users.noreply.github.com>
2026-08-09 22:01:58 +0800

    chore: daily sync 2026-08-09

---
+++ 堆和priority queue/堆和priority queue/p2085最小函数值.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+typedef long long LL;
+const int N = 1e4 + 10;
+int a[N], b[N], c[N];
+int n, m;
+struct node {
+	int f;//计算出的函数值
+	int num;//函数编号
+	int x;//代入值
+	bool operator<(const node& x)const {
+		return f > x.f;
+	}
+};
+int calcu(int i, int x) {
+	return a[i] * x * x + b[i] * x + c[i];
+}
+int main() {
+	cin >> n >> m;
+	priority_queue<node> heap;
+	
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i] >> b[i] >> c[i];
+	}
+	//把1的推进去
+	for (int i = 1; i <= n; i++) {
+		heap.push({ calcu(i,1),i,1 });
+	}
+	//输出
+	while (m--) {
+		auto t = heap.top(); heap.pop();
+		int f = t.f; int num = t.num; int x = t.x;
+		cout << f << " ";
+		heap.push({ calcu(num,x + 1),num,x + 1 });
+	}
+	return 0;
+}
