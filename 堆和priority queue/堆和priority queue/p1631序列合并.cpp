3b8c18e852d92c75ec8acd4fb80e0ce5f8df557b
daotuidezhong <241579381+daotuidezhong@users.noreply.github.com>
2026-08-12 22:02:50 +0800

    chore: daily sync 2026-08-12

--- 堆和priority queue/堆和priority queue/p1631序列合并.cpp
+++ 堆和priority queue/堆和priority queue/p1631序列合并.cpp
@@ -1,42 +1,42 @@
-#include<iostream>
-#include<queue>
-using namespace std;
-const int N = 1e5 + 10;
-int n;
-int a[N], b[N];
-struct node {
-	int sum;//存储和
-	int i, j;//存储a，b的编号
-	bool operator < (const node& x)const {
-		return sum > x.sum;
-	}
-};
-int calcu(int u,int v) {
-		int sum = a[u] + b[v];
-		return sum;
-}
-priority_queue<node> heap;
-int main() {
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
-	}
-	for (int i = 1; i <= n; i++) {
-		cin >> b[i];
-	}
-	//先将第一次的总和结果存入
-	for (int i = 1; i <= n; i++) {
-		int sum = a[1] + b[i];
-		heap.push({ sum,1,i });
-	}
-	//再输出后面的结果
-	while (n--) {
-		auto t = heap.top(); heap.pop();
-		int sum = t.sum; int i = t.i; int j = t.j;
-		cout << sum << " ";
-		if (i + 1 <= n) { //可能会越界
-			heap.push({ calcu(i + 1,j),i + 1,j });
-		}
-	}
-	return 0; 
-}
+//#include<iostream>
+//#include<queue>
+//using namespace std;
+//const int N = 1e5 + 10;
+//int n;
+//int a[N], b[N];
+//struct node {
+//	int sum;//存储和
+//	int i, j;//存储a，b的编号
+//	bool operator < (const node& x)const {
+//		return sum > x.sum;
+//	}
+//};
+//int calcu(int u,int v) {
+//		int sum = a[u] + b[v];
+//		return sum;
+//}
+//priority_queue<node> heap;
+//int main() {
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		cin >> a[i];
+//	}
+//	for (int i = 1; i <= n; i++) {
+//		cin >> b[i];
+//	}
+//	//先将第一次的总和结果存入
+//	for (int i = 1; i <= n; i++) {
+//		int sum = a[1] + b[i];
+//		heap.push({ sum,1,i });
+//	}
+//	//再输出后面的结果
+//	while (n--) {
+//		auto t = heap.top(); heap.pop();
+//		int sum = t.sum; int i = t.i; int j = t.j;
+//		cout << sum << " ";
+//		if (i + 1 <= n) { //可能会越界
+//			heap.push({ calcu(i + 1,j),i + 1,j });
+//		}
+//	}
+//	return 0; 
+//}
---
+++ 堆和priority queue/堆和priority queue/p1878舞蹈课.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+#include<cmath>
+#include<vector>
+using namespace std;
+int n;
+const int N = 2e5 + 10;
+int s[N];//用数字零一标记男女
+bool st[N];//标记已经出队的人
+int pre[N];
+int ne[N],e[N];
+struct node {
+	int d;//技术差
+	int l;//左编号
+	int r;//右编号
+	bool operator <(const node& x) const {
+		if (d != x.d) return d > x.d;
+		else if (l != x.l) return l > x.l;
+		else if (r != x.r) return r > x.r;
+	}
+};
+priority_queue<node>heap;
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		char ch; cin >> ch;
+		if (ch == 'B') s[i] = 1;
+	}
+	for (int i = 1; i <= n; i++) {
+		cin >> e[i];
+		//创建双向链表
+		pre[i] = i - 1;
+		ne[i] = i + 1;
+	}
+	pre[1] = ne[n] = 0;//0表示后面没有元素
+	//1.先把所有的异形差放进堆中
+	for (int i = 2; i <= n; i++) {
+		if (s[i] != s[i - 1]) {
+			heap.push({ abs(e[i] - e[i - 1]), i - 1, i });
+		}
+	}
+	//2.提取结果
+	vector<node> ret;//暂存结果
+	while (heap.size()) {
+		node t = heap.top(); heap.pop();
+		int d = t.d; int l = t.l, r = t.r;
+		if (st[l] || st[r]) continue;
+		ret.push_back(t);
+		st[l] = st[r] = true;
+		ne[pre[l]] = ne[r];
+		pre[ne[r]] = pre[l];
+		int left = pre[l], right = ne[r];
+		if (left&&right&&s[left] != pre[right]) {
+			heap.push({ abs(e[left] - e[right]),left,right});
+		}
+	}
+
+	cout << ret.size() << endl;
+	for (auto& x : ret) {
+		cout << x.l << " " << x.r << endl;
+	}
+	return 0;
+}
